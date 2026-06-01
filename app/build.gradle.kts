b7f0698051e6ae27e1d4dcf9b7cb24c761ca2575
nikola <nikolox1808@gmail.com>
2026-06-01 16:56:35 +0200

    version code 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "rs.nikolaivanovic.imagetoasciiconverter"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
